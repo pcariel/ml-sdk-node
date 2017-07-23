43495780eabe43555c9bcedd522f0709c0596cff
Pablo Cariel <pablocariel@gmail.com>
2017-07-22 21:42:08 -0400

    add model Token

--- src/models/token.ts
+++ src/models/token.ts
@@ -1,2 +1,22 @@
+import { Expose } from "class-transformer";
+
 export class Token {
+
+  @Expose({ name: 'access_token' })
+  accessToken: string;
+
+  @Expose({ name: 'token_type' })
+  tokenType: string;
+  
+  @Expose({ name: 'expires_in' })
+  expired_in: number;
+
+  @Expose()
+  scope: string;
+
+  @Expose({ name: 'refresh_token' })
+  refreshToken: string;
+
+  @Expose({ name: 'user_id' })
+  userId: number;
 }
--- src/services/token.ts
+++ src/services/token.ts
@@ -2,20 +2,23 @@ import { APIRequest } from "../request";
 import { Token } from "../models";
 import { URL_TOKEN, IConfig } from '../config';
 import * as debug from "debug";
+import { plainToClass } from "class-transformer";
 
 const log = debug('MercadoLibre:TokenService');
 
+export type TokenPromise = Promise<Token | Token[]>;
+
 export interface IToken {
-  getAccessToken(code: string): Promise<Token>;
-  refreshAccessToken(accessToken: string): Promise<Token>;
+  getAccessToken(code: string): TokenPromise;
+  refreshAccessToken(accessToken: string): TokenPromise;
 }
 
 export class TokenService extends APIRequest implements IToken {
   constructor(public config: IConfig) {
     super();
   }
-  public async getAccessToken(code: string): Promise<Token> {
-    const uri:string = `${URL_TOKEN}`;
+  public async getAccessToken(code: string): TokenPromise {
+    const uri: string = `${URL_TOKEN}`;
     const options = {
       grant_type: 'authorization_code',
       client_id: this.config.clientId,
@@ -25,9 +28,20 @@ export class TokenService extends APIRequest implements IToken {
     };
 
     log('GET -> AccessToken, Code: ${code}');
-    return this.post(uri, options);
+    return this.post(uri, options)
+      .then((resp) => plainToClass(Token, resp));
   }
-  public async refreshAccessToken(accessToken: string): Promise<Token> {
-    throw new Error("Method not implemented.");
+  public async refreshAccessToken(refreshToken: string): TokenPromise {
+    const uri: string = `${URL_TOKEN}`;
+    const options = {
+      grant_type: 'refresh_token',
+      client_id: this.config.clientId,
+      client_secret: this.config.clientSecret,
+      redirect_uri: this.config.redirectUrl,
+      refresh_token: refreshToken
+    };
+    log('GET -> RefreshToken, ${refreshToken}');
+    return this.post(uri, options)
+      .then((resp) => plainToClass(Token, resp));
   }
 }
